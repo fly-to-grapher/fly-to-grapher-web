71afd4bf4ed83c496f032ea68970db81cbca24ac
mohndkh20 <mondkaled4@gmail.com>
2022-08-04 20:12:20 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/navbar/Nav"
+import Home from "./components/home/Home";
+import Signup from "./components/Signup";
+// import './Style.css';
+
+
+
+
+
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ src/components/home/Home.js
@@ -0,0 +1,115 @@
+import React from "react";
+import './Home.css'
+
+
+const Home = () => {
+    return (
+        <div>
+            <a href="#" className="prev">
+                &lt;
+            </a>
+            <a href="#" className="next">
+                &gt;
+            </a>
+            <h1>Free Stock Photos</h1>
+            <main className="container">
+                <div>
+                    <img
+                        src="https://i.postimg.cc/MGN0PM10/pexels-kelvin-valerio-617278.jpg"
+                        alt=""
+                    />
+                </div>
+                <div className="vertical">
+                    <img
+                        src="https://i.postimg.cc/1RcZhLb8/pexels-maksim-goncharenok-4596641.jpg"
+                        alt=""
+                    />
+                </div>
+                <div className="horizontal">
+                    <img
+                        src="https://i.postimg.cc/YStVt9FZ/pexels-matheus-bertelli-573298.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        src="https://i.postimg.cc/bJ6n560d/a-tattooed-hand-doing-the-sign-for-i-love-you.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        src="https://i.postimg.cc/PJgCG0Cf/pexels-wouter-de-jong-571169.jpg"
+                        alt=""
+                    />
+                </div>
+                <div className="big">
+                    <img
+                        src="https://i.postimg.cc/K8P4YTdz/pexels-life-of-wu-3381013.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img src="https://i.postimg.cc/rFXkR5tt/pic7.png" alt="" />
+                </div>
+                <div className="vertical">
+                    <img src="https://source.unsplash.com/random" alt="" />
+                </div>
+                <div>
+                    <img src="https://picsum.photos/id/29/800" alt="" />
+                </div>
+                <div className="horizontal">
+                    <img src="https://picsum.photos/id/10/800" alt="" />
+                </div>
+                <div>
+                    <img src="https://picsum.photos/id/11/800" alt="" />
+                </div>
+                <div className="big">
+                    <img
+                        src="https://i.postimg.cc/FRY8f6KQ/pexels-andrea-piacquadio-3776169.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        src="https://picsum.photos/id/1/200/300
+"
+                        alt=""
+                    />
+                </div>
+                <div className="horizontal">
+                    <img
+                        src="https://source.unsplash.com/user/erondu/daily
+"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        src="https://source.unsplash.com/weekly?landscape
+"
+                        alt=""
+                    />
+                </div>
+                <div className="big">
+                    <img
+                        src="https://i.postimg.cc/cHyKbrbg/pexels-aleksandar-pasaric-325185.jpg
+"
+                        alt=""
+                    />
+                </div>
+                <div className="explore">Explore</div>
+            </main>
+        </div>
+    )
+}
+
+
+
+export default Home;
+
+
+
+
+
+
