0addfea2fa63b6707f8dbc9d50afe46a59e31de6
mohamed <alyosumohammed@gmail.com>
2022-08-05 16:35:11 +0300

    add singup and login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/navbar/Nav"
 import Home from "./components/home/Home";
 import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login"
+import Videos from "./components/videos/Videos"
 // import './Style.css';
 
 
@@ -17,6 +19,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/videos" element={<Videos />} />
       </Routes>
 
     </BrowserRouter>
---
+++ src/components/videos/Videos.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+const Videos = () => {
+    return (
+        <div>
+            <h1>
+                fvebklrhbtlbehfble ghvkhevghegg
+            </h1>
+        </div>
+    )
+}
+
+
+export default Videos;
