4001f98c11e99a3d45038e729d436dc04a6658a3
mohamed <alyosumohammed@gmail.com>
2022-08-08 21:44:29 +0300

    add upload

--- src/components/upload/Upload.js
+++ src/components/upload/Upload.js
@@ -1,14 +1,39 @@
 import React from "react";
-
+import "./Upload.css"
 
 
 
 const Upload = () => {
     return (
-        <div>
-            <h1>
-                dfghfghjdfghfghrtgyuihrfugiuregrf
-            </h1>
+        <div className="mt-5 mb-5">
+            <div className="file-upload">
+                <button
+                    className="file-upload-btn"
+                    type="button"
+                    onclick="$('.file-upload-input').trigger( 'click' )"
+                >
+                    Add File
+                </button>
+                <div className="image-upload-wrap">
+                    <input
+                        className="file-upload-input"
+                        type="file"
+                        onchange="readURL(this);"
+                        accept="image/*"
+                    />
+                    <div className="drag-text">
+                        <h3>Drag and drop a file or select add Image</h3>
+                    </div>
+                </div>
+                <div className="file-upload-content">
+                    <img className="file-upload-image" src="#" alt="your image" />
+                    <div className="image-title-wrap">
+                        <button type="button" onclick="removeUpload()" className="remove-image">
+                            Remove <span className="image-title">Uploaded Image</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,14 +1,51 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 
 
 const Videos = () => {
     return (
         <div>
-            <h1>
-                fvebklrhbtlbehfble ghvkhevghegg
-            </h1>
+            <div>
+                {/* Start hero*/}
+                <div className="herro">
+                    <div className="content">
+                        {/*     <h1>Royalty Free Stock Photos</h1> */}
+                        <h1>The best free stock photos and videos</h1>
+                        <input type="search" className="find" />
+                    </div>
+                </div>
+                {/* End hero*/}
+                <navbar>
+                    <div>
+                        <ul className="nav justify-content-center mt-3 mb-2">
+                            <Link to="/" className="nav-item" style={{ textDecoration: 'none' }}>
+                                <a className="nav-link" style={{ color: "black" }}><b><h4><u>Home</u></h4></b></a>
+                            </Link>
+                            <Link to="/videos" className="nav-item" style={{ textDecoration: 'none' }}>
+                                <a className="nav-link" style={{ color: "black" }}><b><h4><u>Videos</u></h4></b></a>
+                            </Link>
+                            <Link to="/categories" className="nav-item" style={{ textDecoration: 'none' }}>
+                                <a className="nav-link" style={{ color: "black" }}><b><h4><u>Categories</u></h4></b></a>
+                            </Link>
+
+
+                            {/* <li className="active">Home</li>
+          <li className="active">Videos</li>
+          <li className="active">Categories</li> */}
+                        </ul>
+                    </div>
+                </navbar>
+            </div>
+            <div>
+                <a href="#" className="prev">
+                    &lt;
+                </a>
+                <a href="#" className="next">
+                    &gt;
+                </a>
+                <h1 className="d-flex justify-content-center">Free Stock Videos</h1>
+            </div>
         </div>
     )
 }
