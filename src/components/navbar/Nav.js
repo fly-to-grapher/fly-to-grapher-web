232ff61192fa1dfd4102a947b5352ec8d3526985
mohndkh20 <mondkaled4@gmail.com>
2022-08-09 13:22:16 +0300

    change color nav when scroll

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -69,6 +69,72 @@ const Nav = () => {
               />}
         </Link>
         <div className="d-flex justify-content-center">
+          {
+            nav
+            ? <div
+            className="nav-item dropdown d-flex justify-content-center align-items-center me-4"
+            style={{ color: "#012848" }}
+          >
+            <li
+              className="nav-link dropdown-toggle"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              style={{ color: "#012848" }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="23"
+                height="23"
+                fill="currentColor"
+                className="bi bi-person-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                <path
+                  fillRule="evenodd"
+                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                />
+              </svg>
+            </li>
+            <ul
+              className="dropdown-menu "
+              style={{}}
+              aria-labelledby="navbarDropdown"
+            >
+              <li>
+                {" "}<Link
+                  to="/account"
+                  className="dropdown-item"
+                  style={{ color: "black" }}
+                >
+                  Your profile
+                </Link>
+              </li>
+              <li className="dropdown-item">Log Out</li>
+              <li>
+                {" "}<Link
+                  to="/signup"
+                  className="dropdown-item"
+                  style={{ color: "black" }}
+                >
+                  Sign Up
+                </Link>
+              </li>
+              <li>
+                {" "}<Link
+                  to="/login"
+                  className="dropdown-item"
+                  style={{ color: "black" }}
+                >
+                  Log In
+                </Link>{" "}
+              </li>
+            </ul>
+            
+          </div>
+          :
           <div
             className="nav-item dropdown d-flex justify-content-center align-items-center me-4"
             style={{ color: "white" }}
@@ -111,7 +177,6 @@ const Nav = () => {
                 </Link>
               </li>
               <li className="dropdown-item">Log Out</li>
-
               <li>
                 {" "}<Link
                   to="/signup"
@@ -131,14 +196,26 @@ const Nav = () => {
                 </Link>{" "}
               </li>
             </ul>
+            
           </div>
-          <Link
+          }
+          {
+          nav
+          ?<Link
             to="/upload"
             className="btn "
-            style={{ color: "black", background: "white" }}
+            style={{ color: "white", background: "#012848" }}
           >
             Upload
           </Link>
+          :<Link
+            to="/upload"
+            className="btn "
+            style={{ color: "#012848", background: "white" }}
+          >
+            Upload
+          </Link>
+            }
         </div>
       </div>
     </nav>
