def8069e01900d3da80cd265bdc448258febb06f
mohndkh20 <mondkaled4@gmail.com>
2022-08-17 17:04:40 +0300

    add profile

--- src/components/profile/myProfile.js
+++ src/components/profile/myProfile.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD:src/components/profile/myProfile.js
-=======
 import { useState } from "react";
->>>>>>> 07baa040320f992dc9729b972e1d48eb522b487f:src/components/profile/Profile.js
 import Nav2 from "../navbar/Nav2";
 import { Link } from "react-router-dom"
 import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
@@ -50,15 +47,12 @@ const MyProfile = () => {
             <div className="container-fluid">
                 <div className="row mt-5">
                     <div className="offset-2 col-8 d-flex justify-content-center align-items-center">
-<<<<<<< HEAD:src/components/profile/myProfile.js
                         <img 
                             src='https://firebasestorage.googleapis.com/v0/b/flytographer-a1fa1.appspot.com/o/avatar%2Fprofile-icon-png-908.png?alt=media&token=3d4fc76e-e162-4074-b8d8-d7b4cd8c3ad4'
                             alt="avatar" style={{width:"10em", height: "10em", borderRadius: "50%"}}/>
-=======
                         <img
                             src="https://cdn.dribbble.com/users/772985/screenshots/9247897/media/59b9f4624886350945af7b7fd2ee318f.png?compress=1&resize=400x300&vertical=top"
                             alt="avatar" style={{ width: "10em", height: "10em", borderRadius: "50%" }} />
->>>>>>> 07baa040320f992dc9729b972e1d48eb522b487f:src/components/profile/Profile.js
                     </div>
                     <div className="offset-1 col-1 d-flex justify-content-center align-items-center flex-column gap-3">
                         <Link to='/edit-profile'><ModeEditOutlineOutlinedIcon /></Link>
